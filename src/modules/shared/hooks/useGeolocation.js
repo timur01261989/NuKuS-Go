6c12131280b0cefb2080fe0ffcd3b0f7e19ea419
Timur <timur012689@gmail.com>
2026-03-13 23:16:35 +0500

    123

--- src/modules/shared/hooks/useGeolocation.js
+++ src/modules/shared/hooks/useGeolocation.js
@@ -1,10 +1,11 @@
 import { useCallback, useMemo } from "react";
 
-export default function useGeolocation() {
+export default function useGeoLocation() {
   const getCurrentPosition = useCallback(() => {
     if (typeof window === "undefined" || !navigator.geolocation) {
       return Promise.reject(new Error("Geolocation is not supported"));
     }
+
     return new Promise((resolve, reject) => {
       navigator.geolocation.getCurrentPosition(resolve, reject, {
         enableHighAccuracy: true,
