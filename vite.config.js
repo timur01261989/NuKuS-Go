6148d380a50634441fa3b592e3bba2835ec3e53f
Timur <timur012689@gmail.com>
2026-03-20 19:08:58 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Android uchun nisbiy yo'l
+  base: "./", 
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
